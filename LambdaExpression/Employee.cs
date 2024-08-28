7c290328910917d5bc543f318cbd7ceb3a156316
AwakeningSoul <weathergoals@gmail.com>
2024-08-28 13:27:43 +0100

    Add project files.

---
+++ LambdaExpression/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpression
+{
+    //Employee class with the following properties: Id, First Name, Last Name
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+    }
+}
